660ef4a5a0bfa7b532ac26ecae18626b5cf6f972
=Eliezer Antonio <=eliezerfernandoantonio@gmail.com>
2024-06-11 06:15:09 +0100

    refactor: :recycle: uuid implementation

--- 01-car-dealernship/src/cars/cars.service.ts
+++ 01-car-dealernship/src/cars/cars.service.ts
@@ -1,18 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
+import { v4 as uuid } from 'uuid';
+import { Car } from './interfaces/car.interface';
 @Injectable()
 export class CarsService {
-  private cars = [
-    { id: 1, brand: 'Toyota' },
-    { id: 3, brand: 'Honda' },
-    { id: 3, brand: 'BMW' },
-  ];
+  private cars: Car[] = [{ id: uuid(), brand: 'Toyota', model: 'Corola' }];
 
   public findAll() {
     return this.cars;
   }
 
-  public findOneById(id: number) {
+  public findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car)
@@ -22,6 +19,33 @@ export class CarsService {
   }
 
   public save(body: any) {
+    body.id = uuid();
     this.cars.push(body);
+    return {
+      message: 'Cadastrado com sucesso',
+      car: body,
+    };
+  }
+
+  public delete(id: string) {
+    this.findOneById(id);
+    this.cars = this.cars.filter((c) => c.id !== id);
+
+    return {
+      message: 'Eliminado com sucesso',
+    };
+  }
+
+  public update(body: any) {
+    const { id, brand } = body;
+
+    const car = this.findOneById(id);
+
+    car.brand = brand;
+
+    return {
+      message: 'Atualizado com sucesso',
+      data: { body },
+    };
   }
 }
