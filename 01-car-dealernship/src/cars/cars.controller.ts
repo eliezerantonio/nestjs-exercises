14ec48d6df979e017804842d83bd32a973244d48
=Eliezer Antonio <=eliezerfernandoantonio@gmail.com>
2024-06-11 06:13:59 +0100

    feat: :zap: validate uuid

--- 01-car-dealernship/src/cars/cars.controller.ts
+++ 01-car-dealernship/src/cars/cars.controller.ts
@@ -4,35 +4,39 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCartDto, ListAllEntities } from './dto/cars.dto';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
   @Get()
-  getAllCars() {
+  getAllCars(@Query() query: ListAllEntities) {
     return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
-    return this.carsService.findOneById(+id);
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.findOneById(id);
   }
 
   @Post()
-  create(@Body() body: any) {
-    return body;
+  create(@Body() body: CreateCartDto) {
+    return this.carsService.save(body);
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
-    return body;
+  update(@Param('id', ParseUUIDPipe) id: number, @Body() body: any) {
+    return this.carsService.update(body);
   }
 
   @Delete(':id')
-  delete(@Param('id', ParseIntPipe) id: number) {}
+  delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.delete(id);
+  }
 }
