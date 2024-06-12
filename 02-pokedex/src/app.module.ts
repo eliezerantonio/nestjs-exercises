8ee4f8de29e2bb2d93bed7161736d01c116fbe9d
Eliezer António <eliezerfernandoantonio@gmail.com>
2024-06-12 05:55:37 +0100

    feat: :tada: new projects

---
+++ 02-pokedex/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PokemonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
