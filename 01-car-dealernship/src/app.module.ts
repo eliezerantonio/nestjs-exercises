1bfcf9173fbcbe475744f011e078b1f2e227c5a3
Eliezer António <eliezerfernandoantonio@gmail.com>
2024-06-12 02:56:22 +0100

    feat(brands): :sparkles: added brands resources

--- 01-car-dealernship/src/app.module.ts
+++ 01-car-dealernship/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CarsController } from './cars/cars.controller';
 import { CarsModule } from './cars/cars.module';
 import { CarsService } from './cars/cars.service';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule],
   controllers: [CarsController],
   providers: [CarsService],
   exports: [],
