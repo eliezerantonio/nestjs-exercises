da7ed97d0d1c40a51ae799d044e0a6709d1a1c20
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-06-18 05:04:52 +0100

    refactor(products): :recycle: improve the find one method

--- 03-teslo-shop/src/products/products.controller.ts
+++ 03-teslo-shop/src/products/products.controller.ts
@@ -34,8 +34,11 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
--- 03-teslo-shop/src/products/products.service.ts
+++ 03-teslo-shop/src/products/products.service.ts
@@ -67,8 +67,15 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto,
+    });
+
+    if (!product) throw new NotFoundException(`Product with ${id} not found`);
+
+    return await this.productRepository.save(product);
   }
 
   async remove(id: string) {
