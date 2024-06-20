3fec2f08fae65539010f396f4bf65fcc4a369e33
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-06-20 06:22:30 +0100

    feat: :sparkles: config imageProduct entity

--- 03-teslo-shop/src/products/products.module.ts
+++ 03-teslo-shop/src/products/products.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
 })
 export class ProductsModule {}
