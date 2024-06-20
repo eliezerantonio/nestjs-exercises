cf990d3791c9e3e1abb680733f9ff2258d5ab465
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-06-20 07:04:37 +0100

    feat(products): :ambulance: on delete cascade

--- 03-teslo-shop/src/products/entities/product-image.entity.ts
+++ 03-teslo-shop/src/products/entities/product-image.entity.ts
@@ -9,6 +9,8 @@ export class ProductImage {
   @Column('text')
   url: string;
 
-  @ManyToOne(() => Product, (product) => product.images)
+  @ManyToOne(() => Product, (product) => product.images, {
+    onDelete: 'CASCADE',
+  })
   product: Product;
 }
