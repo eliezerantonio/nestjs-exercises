67b702b0bd3486d83af042701eb7c394cc844957
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-06-21 02:46:19 +0100

    finish implementation upload image

--- 03-teslo-shop/src/app.module.ts
+++ 03-teslo-shop/src/app.module.ts
@@ -1,10 +1,14 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
+
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
@@ -21,6 +25,9 @@ import { FilesModule } from './files/files.module';
       synchronize: true,
     }),
 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ProductsModule,
 
     CommonModule,
